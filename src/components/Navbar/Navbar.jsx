d960316b4130e68232f765ea26d741da22c55dac
viditchawda <viditchawda301@gmail.com>
2023-06-07 23:44:46 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+import { FaSearch } from 'react-icons/fa'
+import Button from '../../components/Button/Button'
+import Avatar from '../Avavtar/Avatar'
+
+const Navbar = () => {
+
+  const User = null
+
+  return (
+    <nav>
+      <div className='navbar'>
+
+        <Link to='/' className='nav-item logo'>
+          <img src={logo} alt="logo" className='logo' />
+        </Link>
+        <Link to='/' className='nav-item nav-btn'>About</Link>
+        <Link to='/' className='nav-item nav-btn'>Product</Link>
+        <Link to='/' className='nav-item nav-btn'>For Teams</Link>
+
+        <form action="">
+          <input type="text" placeholder='Search' />
+          <FaSearch className='icon'/>
+        </form>
+        {
+          User === null ?
+            <Link to='/Auth' className='nav-item nav-links'>Log in</Link> :
+            <>
+              <Link to='/User' style={{color : "White" , textDecoration : "none"}}  className='nav-items'>
+                <Avatar backgroundColor={'#009dff'} borderRadius="50%" px="10px" py ="7px" >M</Avatar>
+              </Link>
+              <Button className="nav-btn">Log out</Button>
+            </>
+        }
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Auth.css'
+const Auth = () => {
+  return (
+    <div>
+        <h1>Auth</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae, voluptatem consectetur, architecto, consequatur ab nesciunt corporis iure aspernatur eaque voluptatum ratione neque dolore reiciendis temporibus. Repellat quo necessitatibus autem!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae, voluptatem consectetur, architecto, consequatur ab nesciunt corporis iure aspernatur eaque voluptatum ratione neque dolore reiciendis temporibus. Repellat quo necessitatibus autem!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae, voluptatem consectetur, architecto, consequatur ab nesciunt corporis iure aspernatur eaque voluptatum ratione neque dolore reiciendis temporibus. Repellat quo necessitatibus autem!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae, voluptatem consectetur, architecto, consequatur ab nesciunt corporis iure aspernatur eaque voluptatum ratione neque dolore reiciendis temporibus. Repellat quo necessitatibus autem!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae, voluptatem consectetur, architecto, consequatur ab nesciunt corporis iure aspernatur eaque voluptatum ratione neque dolore reiciendis temporibus. Repellat quo necessitatibus autem!</p>
+    </div>
+  )
+}
+
+export default Auth
